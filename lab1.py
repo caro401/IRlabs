102ff33230693b284163c98abc8884741df8c689
Sharp_fang <procyonbabel@gmail.com>
2016-04-15 19:21:13 +0000

    remove stop words, initial commit for report

--- lab1.py
+++ lab1.py
@@ -17,12 +17,27 @@ print("File Imported. ")
 print(len(inverted_index), " entries in dictionary.")
 print(num_postings, " total postings.")
 
-# find the 10 most frequent words
+# find the 10 most frequent words and make them the stop words
+# stop_words is a list with ten tuples in the form of (frequecy, word) 
+stop_num = input("How many stop words do you want to exclude? Default value is 10. ")
+try: 
+    stop_num = int(stop_num)
+except ValueError:
+    stop_num = 10 
 stop_words = list()
-# for word in inverted_index:
+for key, value in inverted_index.items():
+    stop_words.append((len(value), key))
 
+stop_words.sort(reverse=True)
 print("Stopwords: ")
-print(stop_words)
+print(stop_words[:stop_num])
 
-query = input("Query: ")
+# update the size of total entries and postings after removing all the stop words
+print(len(inverted_index)-stop_num, "entries in dictionary after the stop words are removed. ")
+stop_words_postings = 0
+for key, value in stop_words[:stop_num]:
+    stop_words_postings += key
+print(num_postings-stop_words_postings, "total postings after the stop words are removed. ")
+
+#query = input("Query: ")
 
